95bd12b33bbe10539949cdad25b0ea32b392fdf8
LEE WEI YING <148225902+emery97@users.noreply.github.com>
2025-02-06 18:04:37 +0800

    installed dependencies

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,94 +1,67 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiVisualstudiocode,
   SiPostman,
   SiSlack,
   SiVercel,
   SiMacos,
+  SiCanva,
+  SiAndroidstudio,
+  SiJira,
 } from "react-icons/si";
-import { FaGithub } from "react-icons/fa";
-import powerBI from "../../Assets/../Assets/power-bi.svg";
-import visualParadigm from "../../Assets/../Assets/visual-paradigm-icon.png";
-import { SiCanva } from "react-icons/si";
-import { FaFigma } from "react-icons/fa";
-import { SiAndroidstudio } from "react-icons/si";
-import { SiJira } from "react-icons/si";
+import { FaGithub, FaFigma } from "react-icons/fa";
+import powerBI from "../../Assets/power-bi.svg";
+import visualParadigm from "../../Assets/visual-paradigm-icon.png";
 
 function Toolstack() {
   const toolDescription = {
-    SiVisualstudiocode: "Visual Studio Code",
+    SiVisualStudioCode: "Visual Studio Code",
     FaGithub: "Github",
     SiPostman: "Postman",
     SiCanva: "Canva",
     FaFigma: "Figma",
-    SiAndroidstudio: "Andriod Studio",
+    SiAndroidstudio: "Android Studio",
     SiJira: "Jira",
   };
   
   const iconMap = {
-    powerBI: <powerBI />,
-    SiVisualstudiocode: <SiVisualstudiocode />,
+    powerBI: <img src={powerBI} alt="Power BI" style={{ width: "4rem", height: "4rem", objectFit: "contain" }} />,
+    // SiVisualStudioCode: <SiVisualStudioCode />,
     FaGithub: <FaGithub />,
     SiPostman: <SiPostman />,
     FaFigma: <FaFigma />,
     SiCanva: <SiCanva />,
     SiAndroidstudio: <SiAndroidstudio />,
     SiJira: <SiJira />,
-    visualParadigm: <visualParadigm />,
+    visualParadigm: <img src={visualParadigm} alt="Visual Paradigm" style={{ width: "50px", height: "auto" }} />,
   };
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px"}}>
-    <Col
-      xs={4}
-      md={2}
-      className="tech-icons"
-      style={{ textAlign: "center", margin: "20px", height: "120px" }}
-    >
-      <div className="tech-icon-container">
-        <img
-          src={powerBI}
-          alt="Power BI"
-          style={{
-            width: "4rem",  // Match the size to other icons using rem
-            height: "4rem", // Consistent height
-            objectFit: "contain", // Maintain aspect ratio
-          }}
-        />
-        <div className="tech-description">Power BI</div> {/* Tooltip text */}
-      </div>
-    </Col>
-    <Col
-      xs={4}
-      md={2}
-      className="tech-icons"
-      style={{ textAlign: "center", margin: "20px", height: "120px" }}
-    >
-      <div className="tech-icon-container">
-      <img
-              src={visualParadigm}
-              alt="Power BI"
-              style={{
-                width: "100%",
-                maxWidth: "50px", 
-                height: "auto",
-              }}
-            />
-        <div className="tech-description">Visual Paradigm</div> {/* Tooltip text */}
-      </div>
-    </Col>
 
-    {/* Loop through tech stack to render icon with hover effect */}
-    {Object.keys(toolDescription).map((tech) => (
-      <Col xs={4} md={2} key={tech} className="tech-icons" style={{ height: "120px" }}>
-        {/* Icon rendering directly from iconMap */}
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons" style={{ textAlign: "center", margin: "20px", height: "120px" }}>
         <div className="tech-icon-container">
-          {iconMap[tech]}
-          <div className="tech-description">{toolDescription[tech]}</div> {/* Tech description */}
+          {iconMap.powerBI}
+          <div className="tech-description">Power BI</div>
         </div>
       </Col>
-    ))}
-  </Row>
+      <Col xs={4} md={2} className="tech-icons" style={{ textAlign: "center", margin: "20px", height: "120px" }}>
+        <div className="tech-icon-container">
+          {iconMap.visualParadigm}
+          <div className="tech-description">Visual Paradigm</div>
+        </div>
+      </Col>
+
+      {/* Loop through tech stack to render icon with hover effect */}
+      {Object.keys(toolDescription).map((tech) => (
+        <Col xs={4} md={2} key={tech} className="tech-icons" style={{ height: "120px" }}>
+          {/* Icon rendering directly from iconMap */}
+          <div className="tech-icon-container">
+            {iconMap[tech]}
+            <div className="tech-description">{toolDescription[tech]}</div> {/* Tech description */}
+          </div>
+        </Col>
+      ))}
+    </Row>
   );
 }
 
